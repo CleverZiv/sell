d881193864da3061f9d4304acba9fcecfd1a5886
CleverZiv <13693371454@163.com>
2019-11-13 02:14:02 +0800

    springboot开发微信点餐项目初始化

---
+++ src/test/java/com/imooc/util/LoggerTest.java
@@ -0,0 +1,18 @@
+package com.imooc.util;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+/**
+ * @Classname LoggerTest
+ * @Date 2019/11/13 1:54
+ * @Autor lengzefu
+ */
+@SpringBootTest
+public class LoggerTest {
+    private final Logger logger = LoggerFactory.getLogger(LoggerTest.class);
+
+}
